543ed3b56c0fb9575066cb6961306c7e54ec4668
Patrick Grey <websemantic@yahoo.co.uk>
2018-12-31 00:19:39 +0000

    Rewrite of search to produce json with headers and found sequences.

--- app.js
+++ app.js
@@ -1,167 +1,82 @@
-var fs = require("fs");
-var es = require("event-stream");
-// var now = require("performance-now");
+// PURPOSE:
+//  Input a search string ("CTAG") and return an array of sets that end with that string.
+//  Each set should include the header info and set characters in an object.
+// ASSUMPTIONS:
+//  - Every set has a header directly below it.
+//  - Subset of characters only
+// NEW FEAURES:
+//  Allow string replacement
+//  Web interface / API with JSON return
 
-var totalLines = 0;
-var newSetCount = 0;
-var stringArray = [];
-var hitArray = [];
-// var currentString;
-var addNewString = false;
+const fileSystem = require("fs");
+const eventStream = require("event-stream");
 
-var sequence = process.argv.slice(2);
+const searchString = process.argv.slice(2)[0];
+let cachedHeader = "";
+let cachedSequence = "";
+let sequenceArray = [];
+let totalSequenceCount = 0;
+let totalMatchingSequenceCount = 0;
 
-// var names = [];
-// var firstNames = [];
-// var dupeNames = {};
-// var dateDonationCount = [];
-// var dateDonations = {};
-// var t0;
-// var t1;
-// var t2;
-// var t3;
-// var t4;
-// var t5;
-// var t6;
-// var t7;
+// Utility function
+const isAtEnd = (_text, _search) => {
+  const textUpper = _text.toUpperCase();
+  const searchUpper = _search.toUpperCase();
+  return (
+    textUpper.indexOf(searchUpper) > -1 &&
+    textUpper.substring(
+      textUpper.length - searchUpper.length,
+      textUpper.length
+    ) === searchUpper
+  );
+};
 
-// TODO: Check for comments (;) and return header as well as sequence.
+// Check search text is at end of current cached sequence and add to result if is.
+const searchCachedSequence = () => {
+  if (isAtEnd(cachedSequence, searchString)) {
+    // ATCAGCAGCAACCTGGAGTAA
+    sequenceArray.push({ header: cachedHeader, set: cachedSequence });
+    totalMatchingSequenceCount++;
+  }
+  cachedSequence = "";
+  cachedHeader = "";
+};
 
-var s = fs
-  // .createReadStream('test.txt')
-  .createReadStream("pig.txt")
-  // .createReadStream('/Users/pxn5096/Downloads/indiv18/itcont.txt')
-  .pipe(es.split())
+const stream = fileSystem
+  .createReadStream("pig.fasta")
+  .pipe(eventStream.split())
   .pipe(
-    es
+    eventStream
       .mapSync(function(line) {
+        // New set found so check the cached set and add to array if passes.
+        if (line.charAt(0) === ">") {
+          searchCachedSequence();
+          cachedHeader = line;
+          totalSequenceCount++;
+          return false;
+        }
         // Blank line, ignore
         if (/^\s*$/.test(line)) {
           return false;
         }
-        // Check for header and whether on first line.
-
-        if (line.charAt(0) === ">" && totalLines != 0) {
-          addNewString = true;
-          totalLines++;
+        // Ignore comments
+        if (line.charAt(0) === ";") {
           return false;
         }
 
-        totalLines++;
+        // TODO: Check if line only contains specified characters: ACTG
 
-        if (addNewString) {
-          // currentString = stringArray[newSetCount];
-          var currentSet = stringArray[newSetCount];
-
-          if (currentSet.indexOf(sequence) > -1) {
-            // ATCAGCAGCAACCTGGAGTAA
-            console.log(currentSet);
-            console.log("hit", currentSet.indexOf(sequence));
-            console.log(currentSet.length - sequence[0].length); // checks that is at end
-          }
-
-          newSetCount++;
-          stringArray[newSetCount] = "";
-          addNewString = false;
-          return false;
-        }
-        // console.time("line count");
-        // t0 = now();
-        // console.log(line);
-        stringArray[newSetCount] += line;
-
-        // Header line, start new string.
-        // console.log(line.charAt(0));
-
-        // // get all names
-        // console.time("names");
-        // t2 = now();
-        // var name = line.split("|")[7];
-        // if (totalLines === 433 || totalLines === 43244) {
-        //   names.push(name);
-        // }
-
-        // // get all first halves of names
-        // console.time("most common first name");
-        // t4 = now();
-        // var firstHalfOfName = name.split(", ")[1];
-
-        // if (firstHalfOfName !== undefined) {
-        //   firstHalfOfName.trim();
-
-        //   // filter out middle initials
-        //   if (firstHalfOfName.includes(" ") && firstHalfOfName !== " ") {
-        //     firstName = firstHalfOfName.split(" ")[0];
-        //     firstName.trim();
-        //     firstNames.push(firstName);
-        //   } else {
-        //     firstNames.push(firstHalfOfName);
-        //   }
-        // }
-
-        // // year and month
-        // console.time("total donations for each month");
-        // t6 = now();
-        // var timestamp = line.split("|")[4].slice(0, 6);
-        // var formattedTimestamp =
-        //   timestamp.slice(0, 4) + "-" + timestamp.slice(4, 6);
-        // dateDonationCount.push(formattedTimestamp);
+        // If no other conditions met, must be sequence line so add to current cached sequence.
+        cachedSequence += line;
+        return false;
       })
       .on("error", function(err) {
         console.log("Error while reading file.", err);
       })
       .on("end", function() {
         console.log("Read entire file.");
-        // console.log(stringArray);
-        console.log(sequence);
-        // t1 = now();
-        // console.log("totalLines", totalLines);
-        // console.timeEnd("line count");
-        // console.log(
-        //   `Performance now line count timing: ` + (t1 - t0).toFixed(3) + `ms`
-        // );
-
-        // console.log(names[432]);
-        // console.log(names);
-        // t3 = now();
-        // console.timeEnd("names");
-        // console.log(
-        //   `Performance now names timing: ` + (t3 - t2).toFixed(3) + `ms`
-        // );
-
-        // most common first name
-        // firstNames.forEach(x => {
-        //   dupeNames[x] = (dupeNames[x] || 0) + 1;
-        // });
-        // var sortedDupeNames = [];
-        // sortedDupeNames = Object.entries(dupeNames);
-
-        // sortedDupeNames.sort((a, b) => {
-        //   return b[1] - a[1];
-        // });
-        // console.log(sortedDupeNames[0]);
-        // t5 = now();
-        // console.timeEnd("most common first name");
-        // console.log(
-        //   `Performance now first name timing: ` + (t5 - t4).toFixed(3) + `ms`
-        // );
-
-        // number of donations per month
-        // dateDonationCount.forEach(x => {
-        //   dateDonations[x] = (dateDonations[x] || 0) + 1;
-        // });
-        // logDateElements = (key, value, map) => {
-        //   console.log(
-        //     `Donations per month and year: ${value} and donation count ${key}`
-        //   );
-        // };
-        // new Map(Object.entries(dateDonations)).forEach(logDateElements);
-        // t7 = now();
-        // console.timeEnd("total donations for each month");
-        // console.log(
-        //   `Performance now donations per month timing: ` +
-        //     (t7 - t6).toFixed(3) +
-        //     `ms`
-        // );
+        console.log(sequenceArray);
+        console.log("Total sequences: ", totalSequenceCount);
+        console.log("Total sequences matching: ", totalMatchingSequenceCount);
       })
   );
